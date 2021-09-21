6facc5c7bb3fe76ab68c304d6ae30a980fbed958
brorae <jhr3239@gmail.com>
2021-09-21 15:40:42 +0900

    210921

--- 1966.cpp
+++ 1966.cpp
@@ -4,17 +4,36 @@
 using namespace std;
 
 int main(){
-    int n,count,index,prior,su;
-    queue<int> que;
-    cin >> n;
-    while(n--){       
-        cin >> count >> index;
-        for (int i=0;i<count;i++){
-            cin >> prior;
-            if (index == i)
-         
-                su = prior;
-            que.push(prior);
+    int t,n,m;
+    cin >> t;
+    for (int i=0;i<t;i++){
+        queue<pair<int,int> > q;
+        priority_queue<int> pq;
+        cin >> n >> m;
+        for (int j=0;j<n;j++){
+            int a;
+            cin >> a;
+            q.push(pair<int,int>(j,a));
+            pq.push(a); 
+        }
+
+        int cnt = 0;
+        while(!q.empty()){
+            int index = q.front().first;
+            int value = q.front().second;
+            q.pop();
+            if (pq.top() == value){
+                pq.pop();
+                cnt++;
+                if (index == m){
+                    cout << cnt << "\n";
+                    break;
+                }
+            }
+            else{
+                q.push(pair<int,int>(index,value));
+            }
         }
     }
+    return 0;
 }
