a7f13410cf7a19ad6ac7dee44c7c595a31770ce6
brorae <johrae_@naver.com>
2021-11-12 22:32:11 +0900

    211112

--- 1920.cpp
+++ 1920.cpp
@@ -6,8 +6,23 @@ using namespace std;
 
 vector<int> v;
 
+int binarySearch(int d)
+{
+    int s = 0; //시작
+    int e = v.size() - 1; //끝
+    int m;
+    while (s <= e) {
+        m = (s + e) / 2;
+        if (v[m] == d) return 1;
+        else if (v[m] > d) e = m - 1;
+        else s = m + 1;
+    }
+    return 0;
+}
+
 int main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
     int n,m;
     cin >> n;
@@ -16,13 +31,12 @@ int main(){
         cin >> num;
         v.push_back(num);
     }
+    sort(v.begin(),v.end());
     cin >> m;
     for (int i=0;i<m;i++){
         int num;
         cin >> num;
-        if (find(v.begin(),v.end(),num) == v.end()) cout << 0;
-        else cout << 1;
-        cout << "\n";
+        cout << binarySearch(num) << "\n";
     }
     return 0;
 }
